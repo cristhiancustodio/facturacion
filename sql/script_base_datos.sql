8f59087b14d29e4d8ddf8c69986b762a7923b6fe
Cristhian Custodio <cryisthian_06@hotmail.com>
2024-09-10 13:36:31 -0500

    actualizacion BD

--- sql/script_base_datos.sql
+++ sql/script_base_datos.sql
@@ -45,3 +45,34 @@ create table moneda(
 )
 
 
+INSERT INTO `bodega` (`id_bodega`, `descripcion`, `estado`) VALUES (1, 'Bodega Peru', '1');
+INSERT INTO `bodega` (`id_bodega`, `descripcion`, `estado`) VALUES (2, 'Bodega Chile', '1');
+INSERT INTO `bodega` (`id_bodega`, `descripcion`, `estado`) VALUES (3, 'Bodega Colombia', '1');
+INSERT INTO `bodega` (`id_bodega`, `descripcion`, `estado`) VALUES (4, 'Bodega Mexico', '1');
+
+
+INSERT INTO `material_producto` (`id_materialProducto`, `descripcion`, `estado`) VALUES (10, 'Plástico', '1');
+INSERT INTO `material_producto` (`id_materialProducto`, `descripcion`, `estado`) VALUES (11, 'Metal', '1');
+INSERT INTO `material_producto` (`id_materialProducto`, `descripcion`, `estado`) VALUES (12, 'Madera', '1');
+INSERT INTO `material_producto` (`id_materialProducto`, `descripcion`, `estado`) VALUES (13, 'Vidrio', '1');
+INSERT INTO `material_producto` (`id_materialProducto`, `descripcion`, `estado`) VALUES (14, 'Textil', '1');
+
+
+INSERT INTO `moneda` (`id_moneda`, `descripcion`, `estado`) VALUES (1, 'Soles', '1');
+INSERT INTO `moneda` (`id_moneda`, `descripcion`, `estado`) VALUES (2, 'Peso chileno', '1');
+INSERT INTO `moneda` (`id_moneda`, `descripcion`, `estado`) VALUES (3, 'Peso colombiano', '1');
+INSERT INTO `moneda` (`id_moneda`, `descripcion`, `estado`) VALUES (4, 'Peso mexicano', '1');
+
+
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (1, 'Lima', 1, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (2, 'Arequipa', 1, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (3, 'Cuzco', 1, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (4, 'Huaraz', 1, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (5, 'Santiago', 2, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (6, 'Arica', 2, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (7, 'Antofagasta', 2, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (8, 'Medellin', 3, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (9, 'Bogota', 3, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (10, 'Cucuta', 3, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (11, 'Jalisco', 4, '1');
+INSERT INTO `sucursal` (`id_sucursal`, `descripcion`, `id_bodega`, `estado`) VALUES (12, 'Ciudad de México', 4, '1');
